296a894e14885d0d9731293455cf8e0050b6a73c
alexia <me@alexia.lol>
2023-03-29 01:25:56 +0200

    revert: we don't need this tweak on iOS 15, it just causes issues
    Ref: d60be29

--- main.m
+++ main.m
@@ -6,10 +6,6 @@ int main(int argc, char *argv[], char *envp[]) {
     pid_t pid;
     int status;
     const char* args[] = {"killall", "-9", "SpringBoard", NULL};
-#ifndef ROOTLESS
     posix_spawn(&pid, "/usr/bin/killall", NULL, NULL, (char* const*) args, NULL);
-#else
-    posix_spawn(&pid, "/var/jb/usr/bin/killall", NULL, NULL, (char* const*) args, NULL);
-#endif
     waitpid(pid, &status, WEXITED);
 }
